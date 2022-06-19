902c30cd05c2e771131a9e8fc3733b14da4b326c
seihmd <7434823+seihmd@users.noreply.github.com>
2022-06-19 14:31:15 +0900

    setup tools

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.json'],
+  },
+  ignorePatterns: ['.eslintrc.*'],
+  plugins: ['@typescript-eslint', 'jest', 'import'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: './',
+      },
+    },
+  },
+  rules: {
+    '@typescript-eslint/ban-types': [
+      'error',
+      {
+        types: {
+          Function: false,
+          Object: false,
+        },
+        extendDefaults: true,
+      },
+    ],
+  },
+};
---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  moduleDirectories: ['node_modules', 'src'],
+  roots: ['<rootDir>/src', '<rootDir>/test'],
+  testMatch: [
+    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)',
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+};
