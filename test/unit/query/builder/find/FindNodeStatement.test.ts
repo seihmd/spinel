2892f5a13b4cbc226170dfc85f415c5dc620da4c
seihmd <7434823+seihmd@users.noreply.github.com>
2022-12-18 23:08:48 +0900

    fix test

--- test/unit/query/builder/find/FindNodeStatement.test.ts
+++ test/unit/query/builder/find/FindNodeStatement.test.ts
@@ -1,15 +1,18 @@
 import { NodeLabel } from 'domain/node/NodeLabel';
 import { OrderByQueries } from 'query/builder/orderBy/OrderByQueries';
 import { NodeLiteral } from 'query/literal/NodeLiteral';
+import { PositiveInt } from '../../../../../src/domain/type/PositiveInt';
 import { FindNodeStatement } from '../../../../../src/query/builder/find/FindNodeStatement';
+import { LimitClause } from '../../../../../src/query/clause/LimitClause';
 import { WhereStatement } from '../../../../../src/query/clause/where/WhereStatement';
 
-describe(`${FindNodeStatement.name}`, () => {
+describe(`FindNodeStatement`, () => {
   test('get', () => {
     const findNodeStatement = new FindNodeStatement(
       new NodeLiteral('u', new NodeLabel('User'), null),
       null,
-      new OrderByQueries([])
+      new OrderByQueries([]),
+      null
     );
 
     expect(findNodeStatement.get()).toBe('MATCH (u:User) RETURN u{.*} AS _');
@@ -19,11 +22,25 @@ describe(`${FindNodeStatement.name}`, () => {
     const findNodeStatement = new FindNodeStatement(
       new NodeLiteral('u', new NodeLabel('User'), null),
       new WhereStatement('u.id = $id'),
-      new OrderByQueries([])
+      new OrderByQueries([]),
+      null
     );
 
     expect(findNodeStatement.get()).toBe(
       'MATCH (u:User) WHERE u.id = $id RETURN u{.*} AS _'
+    );
+  });
+
+  test('with Limit', () => {
+    const findNodeStatement = new FindNodeStatement(
+      new NodeLiteral('u', new NodeLabel('User'), null),
+      new WhereStatement('u.id = $id'),
+      new OrderByQueries([]),
+      new LimitClause(new PositiveInt(3))
+    );
+
+    expect(findNodeStatement.get()).toBe(
+      'MATCH (u:User) WHERE u.id = $id RETURN u{.*} AS _ LIMIT 3'
     );
   });
 });
