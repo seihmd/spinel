b34c400832c4bc605d3fd1237274be11fcf594e0
seihmd <7434823+seihmd@users.noreply.github.com>
2022-12-30 20:46:53 +0900

    Update find-N-:R-:N-graphs.test.ts

--- test/integration/find/find-N-:R-:N-graphs.test.ts
+++ test/integration/find/find-N-:R-:N-graphs.test.ts
@@ -1,6 +1,5 @@
 import { Graph } from 'decorator/class/Graph';
 import { NodeEntity } from 'decorator/class/NodeEntity';
-import { GraphBranch } from 'decorator/property/GraphBranch';
 import { GraphNode } from 'decorator/property/GraphNode';
 import { Primary } from 'decorator/property/Primary';
 import 'reflect-metadata';
@@ -26,38 +25,13 @@ class Item {
   }
 }
 
-@NodeEntity()
-class Tag {
-  @Primary() private id: string;
-
-  constructor(id: string) {
-    this.id = id;
-  }
-}
-
-@Graph('item')
-class ItemTags {
-  @GraphNode() private item: Item;
-
-  @GraphBranch(Tag, 'item-[:HAS_TAG]->.') private tags: Tag[];
-
-  constructor(item: Item, tags: Tag[]) {
-    this.item = item;
-    this.tags = tags;
-  }
-}
-
-@Graph('shop')
-class ShopItemTags {
+@Graph('shop-[:HAS_STOCK]->(:Item)')
+class ShopHavingStock {
   @GraphNode()
   private shop: Shop;
 
-  @GraphBranch(ItemTags, 'shop-[:HAS_STOCK]->.item')
-  private itemTags: ItemTags[];
-
-  constructor(shop: Shop, itemTags: ItemTags[]) {
+  constructor(shop: Shop) {
     this.shop = shop;
-    this.itemTags = itemTags;
   }
 }
 
@@ -69,12 +43,12 @@ describe('Find N-:R-:N graphs', () => {
   beforeAll(async () => {
     const shop = await neo4jFixture.addNode('Shop', { id: id.get('shop') });
     const item = await neo4jFixture.addNode('Item', { id: id.get('item') });
-    const tag1 = await neo4jFixture.addNode('Tag', { id: id.get('tag1') });
-    const tag2 = await neo4jFixture.addNode('Tag', { id: id.get('tag2') });
+
+    const shop2 = await neo4jFixture.addNode('Shop', {
+      id: id.get('shopNoStock'),
+    });
 
     await neo4jFixture.addRelationship('HAS_STOCK', {}, shop, item, '->');
-    await neo4jFixture.addRelationship('HAS_TAG', {}, item, tag1, '->');
-    await neo4jFixture.addRelationship('HAS_TAG', {}, item, tag2, '->');
   });
 
   afterAll(async () => {
@@ -85,25 +59,18 @@ describe('Find N-:R-:N graphs', () => {
   test('find', async () => {
     const query = qd
       .builder()
-      .find(ShopItemTags)
+      .find(ShopHavingStock)
       .where('shop.id = $shopId')
       .buildQuery({ shopId: id.get('shop') });
 
     expect(query.getStatement()).toBe(
-      'MATCH (n0:Shop) WHERE n0.id = $shopId ' +
-      'RETURN {shop:n0{.*},' +
-      'itemTags:[(n0)-[b0_r2:HAS_STOCK]->(b0_n4:Item)|{item:b0_n4{.*},' +
-      'tags:[(b0_n4)-[b0_b0_r2:HAS_TAG]->(b0_b0_n4:Tag)|b0_b0_n4{.*}]}]} ' +
-      'AS _'
+      'MATCH (n0:Shop)-[r2:HAS_STOCK]->(n4:Item) ' +
+        'WHERE n0.id = $shopId ' +
+        'RETURN {shop:n0{.*}} AS _'
     );
 
     expect(await query.run()).toStrictEqual([
-      new ShopItemTags(new Shop(id.get('shop')), [
-        new ItemTags(new Item(id.get('item')), [
-          new Tag(id.get('tag2')),
-          new Tag(id.get('tag1')),
-        ]),
-      ]),
+      new ShopHavingStock(new Shop(id.get('shop'))),
     ]);
   });
 });
